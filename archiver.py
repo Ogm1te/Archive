dba3fc7a807211b727674cebc10b2f2c0f087077
Ivanov Timofey <tema.ivanov.2007@inbox.ru>
2026-04-08 10:18:33 +0300

    Add feat: Added plan for archive working + future package for deflate and inflate bugs: it doesn't work now

---
+++ archiver.py
@@ -0,0 +1,30 @@
+import sys
+import deflate
+import inflate
+
+args = sys.argv[1:]
+folder_for_archive = ""
+archive_for_unpackage = ""
+file_to_add = ""
+file_to_rm = ""
+
+# создание архива
+for elems_of_input in range((args-2)):
+    if args[elems_of_input] == "create":
+        with open() as archiver:
+
+# распаковка архива
+for elems_of_input in range((args-2)):
+    if args[elems_of_input] == "unpack":
+        elems_of_input =
+        with open() as archiver:
+
+# добавление файла в архив
+for elems_of_input in range((args-2)):
+    if args[elems_of_input] == "add":
+        with open() as archiver:
+
+# удаление файла из архива
+for elems_of_input in range((args-2)):
+    if args[elems_of_input] == "rm":
+        with open() as archiver:
