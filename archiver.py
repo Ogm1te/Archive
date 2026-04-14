1b1b2b9194a69b2c47ef4843e4964508158d1946
Ivanov Timofey <tema.ivanov.2007@inbox.ru>
2026-04-14 19:49:05 +0300

    aaa

--- archiver.py
+++ archiver.py
@@ -1,6 +1,5 @@
 import sys
-import deflate
-
+from commands import *
 
 args = sys.argv[1:]
 folder_for_packing = ""
@@ -11,40 +10,38 @@ path = ""
 
 # Ошибки
 if len(args) < 3 or len(args) > 3:
-    sys.exit("Error: incorrect number of parameters entered")
-if not args[2].startswith('\\'):
-    sys.exit("Error: incorrect input")
+    print("Error: incorrect number of parameters entered")
+    sys.exit(1)
+if (args[2][1:2] != ":"):
+    print("Error: incorrect input")
+    sys.exit(1)
 
 
 # создание архива
 for elems_of_input in range(len(args)-2):
     if args[elems_of_input] == "create":
         folder_for_packing = args[elems_of_input+1] 
-        with open() as archiver:
+
 
 # распаковка архива
-for elems_of_input in range(len(args)-2):
-    if args[elems_of_input] == "unpack" and args[elems_of_input+2].startswith('\\'):
-        archive_for_unpackage = args[elems_of_input+1]
-        path = args[elems_of_input + 2]
-        with open(f"{path}", 'r') as archiver:
-            Unpack(args[elems_of_input+1], path)
-        break
-    else:
-        sys.exit("Error: incorrect input")
+if args[0] == "unpack":
+    archive_for_unpackage = args[1]
+    path = args[2]
+    with open(f"{path}", 'rb') as archiver:
+        Unpack(archive_for_unpackage, path) #куда распаковываем, какой файл
+else:
+    print("Error: incorrect input")
+    sys.exit(1)
 
 # добавление файла в архив
 for elems_of_input in range(len(args)-2):
     if args[elems_of_input] == "add":
-        with open() as archiver:
+        break
 
 # удаление файла из архива
 for elems_of_input in range(len(args)-2):
-    if args[elems_of_input] == "rm":
-        with open() as archiver:
-
+    break
 '''
 библиотеку архивирования + добавление
 библиотеку разархивирования + удаление
-
 '''
---
+++ commands.py
@@ -0,0 +1,6 @@
+import os
+
+def Unpack(unpackage_file: str, path: str):
+    mkdir(f"{unpackage_file}")
+    
+        
