407fdbe8812b783bc883695c4b32cc01b416bf89
Ivanov Timofey <tema.ivanov.2007@inbox.ru>
2026-05-22 20:24:52 +0300

    feat: доделана функция add bugs: при попытке добавить ещё 1 файл в архив начинаются проблемы с файлами

--- commands.py
+++ commands.py
@@ -6,21 +6,20 @@ CHUNK = 4096
 SIGNA = b'ABOBAHEH'
 
 def Create(packing_file: str, folder_path: str):
-    if os.path.isdir(folder_path): #массив с всеми файлами папки
+    if os.path.isdir(folder_path): #массив со всеми файлами папки
         files = [file for file in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, file))]
-        #основная часть создания файла
+
         with open(packing_file, 'wb') as file_to_archive:
             file_to_archive.write(struct.pack('<8s I', SIGNA, len(files))) 
-            
-            for i in files: 
-                file_path = os.path.join(folder_path, i)
+            #записываем каждый файл в архив
+            for elem in files: 
+                file_path = os.path.join(folder_path, elem)
                 size = os.path.getsize(file_path)
-                name = i.encode('utf-8')
+                name = elem.encode('utf-8')
 
-                #информация для работы с данными
                 file_to_archive.write(struct.pack('<Q', size))
                 file_to_archive.write(struct.pack('<H', len(name)))
-                file_to_archive.write(name) #запись файла
+                file_to_archive.write(name)
 
                 with open(file_path, 'rb') as input_file:
                     while True: 
